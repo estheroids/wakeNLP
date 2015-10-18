6471d0234ea6f3cc15ffaa662250e09903f0864c
spencerhitch <spencerhitch@gmail.com>
2015-10-18 13:02:31 -0700

    German, French and italian wordlists added

--- scripts/italian.py
+++ scripts/italian.py
@@ -1,16 +1,11 @@
 import nltk
+import re
 from nltk import word_tokenize
 
-write_file = open('../res/it-words.txt','wb')
+write_file = open('../res/it-words.txt','w')
 
 with open('../res/italian.txt','r') as f:
-    data = f.read().decode('utf8')
-    tokens = word_tokenize(data)
-    tokens = [w.lower() for w in nltk.Text(tokens)]
-    unique = sorted(list(set(tokens)))
-    for word in unique:
-      write_file.write(word.encode('utf8') + "\n")
-
-    
-
-    
+  data = f.read() #.decode('utf8')
+  tokens = re.findall("[^\W\d]{3,}", data, re.U) 
+  for word in tokens:
+    write_file.write(word + '\n')
